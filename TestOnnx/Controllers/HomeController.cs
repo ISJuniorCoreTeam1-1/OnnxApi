6e356fadc1a28d26b7b96a8ce38b941ab41e7b51
jackenelson <jn375@byu.edu>
2023-04-12 15:57:57 -0600

    Add project files.

---
+++ TestOnnx/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace TestOnnx.Controllers
+{
+    [ApiController]
+    [Route("/score")]
+    public class InferenceController : ControllerBase
+    {
+        private InferenceSession _session;
+
+        public InferenceController(InferenceSession session)
+        {
+            _session = session;
+        }
+
+        [HttpPost]
+        public ActionResult Score(BoneData data)
+        {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
+            });
+            Tensor<string> score = result.First().AsTensor<string>();
+            var categories = new[] { "E", "W" };
+            int predictionIndex = Array.IndexOf(score.ToArray(), score.Max());
+            var prediction = new Prediction { PredictedValue = categories[predictionIndex] }; ;
+            result.Dispose();
+            return Ok(prediction);
+        }
+    }
+}
+
